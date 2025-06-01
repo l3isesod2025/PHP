15948831377f702683d2163ed522e1e3376283eb
GESTIONF <l3isesod2025@gmail.com>
2025-06-01 15:00:25 +0200

    Nous avons travaillé sur le Formulaire Produit

---
+++ GestionInscription/FormProd.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire Produit</title>
+</head>
+<body>
+    <h1>FORMULAIRE PRODUIT</h1>
+    <form action="">
+        <label for="">Article</label>
+        <input type="text"><br><br>
+        <label for="">Prix</label>
+        <input type="number"><br><br>
+        <input type="submit" value="Envoyer">
+    </form>
+    
+</body>
+</html>

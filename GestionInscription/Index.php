8185b37b2069cc59d67abe38303ec6e58de0a814
l3isesod2025 <l3isesod2025@gmail.com>
2025-06-01 12:21:59 +0200

    Transmission du projet initial

---
+++ GestionInscription/Index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>GITHUB</title>
+</head>
+<body>
+	<h1>BONJOUR L3 ISESOD 2025</h1>
+</body>
+</html>

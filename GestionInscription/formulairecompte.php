dfa9b4bab35bbdd8c17fbf36e90e12290f5163fd
l3isesod2025@gmail.com <l3isesod2025@gmail.com>
2025-06-01 14:51:47 +0200

    Nous avons effectué ce travail KWANGABA OSWALD et ERNEST KAWAYA

---
+++ GestionInscription/formulairecompte.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire compte</title>
+</head>
+<body>
+    <h1>FORMULAIRE COMPTE</h1>
+
+    <form action="">
+        <label for="">Num de compte</label>
+        <input type="text"><br><br>
+        <label for="">Désignation</label>
+        <input type="text"><br><br>
+        <label for="">Date de payement</label>
+        <input type="text"><br><br>
+        <input type="submit" name="" id="" value="Envoyer">
+        <p>Merci</p>
+
+    </form>
+    
+</body>
+</html>

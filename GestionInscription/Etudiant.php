eda8be4b83d0b55cf492e90c0d7fa00b06d17a82
l3isesod2025@gmail.com <l3isesod2025@gmail.com>
2025-06-01 14:13:57 +0200

    TD SUR ENVOIE DE FORMULAIRE ETUDIANT DANS REPOSITORY PHP EFFECTUER PAR NDEKO YOSHUA HERITIER

---
+++ GestionInscription/Etudiant.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+  
+    <h1>FORMULAIRE ETUDIANT</h1>
+    <form action="">
+       
+        <label for="">NOM: </label>
+        <input type="text"placeholder="Entrez ici votre nom"><br><br>
+        <label for="">POST-NOM: </label>
+        <input type="text" placeholder="Entrez ici votre post-nom"><br> <br>
+        <label for="">PRENOM: </label> 
+        <input type="text" placeholder="Entrez ici votre prenom"><br><br>
+        <label for="">SEXE: </label>
+        <select name="sexe" id=""><option value="Masculin">Masculin</option>
+             <option value="Féminin">Féminin</option>
+        
+        </select><br><br>
+        <input type="submit" value="Soumettre"> 
+
+    </form>
+  
+</body>
+</html>
